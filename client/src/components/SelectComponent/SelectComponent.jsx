896d30afa30ef32ba73a0b7c68a35bcfa79029f9
Moulaye dagnon <moulayedagnon@gmail.com>
2025-06-23 18:45:23 +0100

    add

--- client/src/components/SelectComponent/SelectComponent.jsx
+++ client/src/components/SelectComponent/SelectComponent.jsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState } from "react";
 import {
   Label,
   Listbox,
@@ -35,9 +34,7 @@ export default function SelectComponent({
         <ListboxButton className="grid w-full cursor-default grid-cols-1 rounded-md bg-white py-1.5 pr-2 pl-3 text-left text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
           <span className="col-start-1 row-start-1 flex items-center gap-3 pr-6">
             <span className="block truncate">
-              {items.length > 0 && items[value - 1]
-                ? items[value - 1].nom
-                : placeholder}
+              {items.find((item) => item.id === value)?.nom || placeholder}
             </span>
           </span>
           <ChevronUpDownIcon
