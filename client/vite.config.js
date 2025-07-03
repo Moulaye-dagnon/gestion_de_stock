5baf3d8b05c437ae4766129d65ea9b001f0a9de7
Moulaye dagnon <moulayedagnon@gmail.com>
2025-07-03 12:21:49 +0100

    add

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,9 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    allowedHosts: [
-      "8f9a-105-68-189-109.ngrok-free.app", // ← ajoute cette ligne
-    ],
-  },
 });
